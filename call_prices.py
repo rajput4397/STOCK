f426077c8d58c27982458ab4b8d6d53cea631e41
ADMIN <shivamrajpoot4397@gmail.com>
2025-12-18 02:18:44 +0530

    hi

--- call_prices.py
+++ call_prices.py
@@ -1,6 +1,30 @@
 import pandas
 from prices import *
 
+import pandas_market_calendars as mcal
+from datetime import date
+import sys
+
+today = date.today()
+
+def is_nse_trading_day(check_date: date) -> bool:
+    """
+    Returns True if NSE is open on the given date
+    """
+    nse = mcal.get_calendar("NSE")
+
+    schedule = nse.schedule(
+        start_date=check_date,
+        end_date=check_date
+    )
+
+    return not schedule.empty
+
+if not is_nse_trading_day(today):
+    print("Market closed. Skipping.")
+    sys.exit(0)
+
+
 df=pd.read_csv('hello.csv')
 
 
